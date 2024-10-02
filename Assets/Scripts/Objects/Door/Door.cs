f447a4bd6342060a446039d42c219db078f551b5
Walus-dummies <walter.bolanos@uniat.edu.mx>
2024-10-02 14:09:46 -0600

    Animación puerta

--- Assets/Scripts/Objects/Door/Door.cs
+++ Assets/Scripts/Objects/Door/Door.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Variables publicas
+    public AudioSource audioSource;
+    public AudioClip openingSound, closingSound, lockedSound;
+
+    // Variables privadas
+    private bool isOpen = false;
+    [SerializeField] private bool isLocked;
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // funcion de interaccion
+    public void Interaction()
     {
-        
+        if (!isLocked)
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+            }
+            else
+            {
+                isOpen = true;
+            }
+            UpdateAnimation();
+        }
+        else
+        {
+            audioSource.clip = lockedSound;
+            audioSource.Play();
+        }
+    }
+
+    // funcion para reproducir el sonido
+    public void PlaySound()
+    {
+        if (isOpen)
+        {
+            audioSource.clip = openingSound;
+        }
+        else
+        {
+            audioSource.clip = closingSound;
+        }
+        audioSource.Play();
+    }
+
+    // funcion para cambiar el estado Open de la puerta
+    public void SetIsOpen(bool _state)
+    {
+        isOpen= _state;
+        UpdateAnimation();
+    }
+
+    // funcion para cambiar el estado Locked de la puerta
+    public void SetIsLocked(bool _state)
+    {
+        isLocked = _state;
+    }
+
+    public void UpdateAnimation()
+    {
+        animator.SetBool("isOpen", isOpen);
     }
 }
